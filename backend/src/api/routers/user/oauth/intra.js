c6fbbb59c7d03e7368b99dbf5d1de7dab37669e5
pkolomiy <pkolomiy@gmail.com>
2018-11-16 13:34:41 +0200

    Changed redirect uri for intra oauth

--- backend/src/api/routers/user/oauth/intra.js
+++ backend/src/api/routers/user/oauth/intra.js
@@ -25,7 +25,7 @@ export const intra = model => (req, res, next) => {
         'client_id': '5b2ec6bcbe8d7d9fa32d6129854aa36ea010afa550ec096b3733bc8cf388d0a7',
         'client_secret': 'b4a92aa14e861ade4033ecb3e15f674fc2a38f925f969a267ee0f99603129f4f',
         'code': code,
-        'redirect_uri': 'http://localhost:8080/oauth',
+        'redirect_uri': 'http://localhost:8080/intra',
     })
         .then(response => axios.get('https://api.intra.42.fr/v2/me', {
             headers: {'Authorization': `Bearer ${response.data.access_token}`}
