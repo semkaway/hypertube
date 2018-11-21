8bb722ede9126e40bfe56b1ab08a2bdbd3730281
pkolomiy <pkolomiy@gmail.com>
2018-11-21 22:09:08 +0200

    GET `/api/user/data/:token` is done

---
+++ backend/src/api/routers/user/controllers/data.js
@@ -0,0 +1,18 @@
+export const data = model => (req, res, next) => {
+    model.findById(req.id)
+        .then(user => {
+            if (user === null) {
+                res.status(200).json({"success": false, "message": "Invalid token"});
+            }
+            let body = (({first, last, email, pendingEmail, image}) =>
+                ({first, last, email, pendingEmail, image}))(user);
+
+            body.intra  = user.intraId !== null;
+            body.github = user.githubId !== null;
+            res.status(200).json({
+                "success": true,
+                ...body
+            })
+        })
+        .catch(error => next(error));
+};
--- backend/src/api/routers/user/controllers/index.js
+++ backend/src/api/routers/user/controllers/index.js
@@ -1,9 +1,10 @@
 import {createUser} from './create'
+import {data} from './data'
 import {checkEmail} from "./check-email"
 import {activateUser} from "./activate"
 import {sendActivation} from "./send-activation";
 import {login} from "./login"
-import {beforeUserSave, User} from '../models'
+import {beforeUserSave, decodeToken, User} from '../models'
 import express from "express";
 
 export const mainRouter = express.Router();
@@ -17,3 +18,5 @@ mainRouter.route('/activate').post(activateUser(User));
 mainRouter.route('/send-activation').post(sendActivation(User));
 
 mainRouter.route('/login').post(login(User));
+
+mainRouter.route('/data/:token').get(decodeToken, data(User));
