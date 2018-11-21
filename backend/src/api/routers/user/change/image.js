3c91b88d52f50dab07ea77df580c660915d982d9
pkolomiy <pkolomiy@gmail.com>
2018-11-21 18:12:12 +0200

    PUT `/api/user/change/image` is done

---
+++ backend/src/api/routers/user/change/image.js
@@ -0,0 +1,16 @@
+export const image = model => (req, res, next) => {
+    if (req.body.image === undefined) {
+        throw new Error("Require 'image' field");
+    }
+    model.findById(req.id)
+        .then(user => {
+            if (user === null) {
+                res.status(200).json({"success": false, "message": 'Invalid token'});
+            } else {
+                user.image = req.body.image;
+                user.save().then(() => res.status(201).json({"success": true}))
+                    .catch(error => next(error));
+            }
+        })
+        .catch(error => next(error));
+};
