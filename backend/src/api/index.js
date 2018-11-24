6bff5c9c5a8ae35fcfbc7af6976ec22d7b037135
pkolomiy <pkolomiy@gmail.com>
2018-11-25 00:26:47 +0200

    GET `api/stream/:magnet` is done

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import {userRouter} from './routers/user'
+import {streamRouter} from './routers/stream'
 
 export const restRouter = express.Router();
 
 restRouter.use('/user', userRouter);
+
+restRouter.use('/stream', streamRouter);
---
+++ backend/src/api/routers/stream/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import torrentStream from "torrent-stream"
+
+export const streamRouter = express.Router();
+
+streamRouter.route('/:magnet').get((req, res) => {
+    const engine = torrentStream(req.params.magnet, {
+        connections: 1000,
+        uploads: 1000,
+        tmp: '/tmp/stream/torrent',
+        path: '/tmp/stream/movies'
+    });
+    engine.on('ready', () => {
+        const video = engine.files.reduce((f1, f2) => f1.length > f2.length ? f1 : f2);
+        const range = req.headers.range ? req.headers.range.substring(6).split('-') : [0, video.length - 1];
+        const start = parseInt(range[0]),
+              end = parseInt(range[1] || video.length - 1);
+        res.writeHead(206, {
+            'Content-Range': `bytes ${start}-${end}/${video.length}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': end - start + 1,
+            'Content-Type': 'video/mp4'
+        });
+        video.createReadStream({start: start, end: end}).pipe(res);
+    });
+});
