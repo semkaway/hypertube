303ad1a6b2beb0af92ae9980de6674486d27c294
pkolomiy <pkolomiy@gmail.com>
2018-11-28 00:37:06 +0200

    modified DELETE `/api/user/delete/self` to work as `hide` not `remove`

--- backend/src/api/routers/user/delete/self.js
+++ backend/src/api/routers/user/delete/self.js
@@ -1,5 +1,18 @@
 export const self = model => (req, res, next) => {
-    model.findByIdAndRemove(req.id)
-        .then(() => res.status(201).json({"success": true}))
+    model.findById(req.id)
+        .then(user => {
+            user.email = null;
+            user.pendingEmail = null;
+            user.password = null;
+            user.intraId = null;
+            user.githubId = null;
+            user.activated = false;
+            user.activationToken = null;
+            user.resetPasswordToken = null;
+            user.comments = null;
+            user.save()
+                .then(() => res.status(201).json({"success": true}))
+                .catch(error => next(error));
+        })
         .catch(error => next(error));
 };
--- backend/src/api/routers/user/rootRouter/commented-movies.js
+++ backend/src/api/routers/user/rootRouter/commented-movies.js
@@ -1,7 +1,7 @@
 export const commentedMovies = model => (req, res, next) => {
     model.findById(req.params.id)
         .then(user => {
-            if (user === null) {
+            if (user === null || user.activated === false) {
                return res.status(200).json({"success": false, "message": "Invalid id"});
             }
             res.status(200).json({
