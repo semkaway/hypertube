760f969e15d862de74072b7913bc7daf166d04f3
Valentin Tolochko <vtolochk@gmail.com>
2018-12-04 19:10:09 +0200

    [DEL] redundant comment

--- frontend/src/components/Activation.vue
+++ frontend/src/components/Activation.vue
@@ -72,9 +72,8 @@ export default {
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  font-size: 1.5rem;
-}
+  h1 {
+    font-size: 1.5rem;
+  }
 </style>
