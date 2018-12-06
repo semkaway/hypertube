748331c3b7b2fd023e8a9fc3bb83c04544e6c445
Valentin Tolochko <vtolochk@gmail.com>
2018-12-06 15:41:13 +0200

    [ADD] snackbar component

---
+++ frontend/src/components/Snackbar.vue
@@ -0,0 +1,30 @@
+<<template>
+    <v-snackbar v-model="active" :bottom="y === 'bottom'" :left="x === 'left'" :right="x === 'right'" :top="y === 'top'" :timeout='timeout'>
+		{{text}}
+    	<v-btn color="pink" flat @click="closeSnackBar">Close</v-btn>
+    </v-snackbar>
+</template>
+
+<script>
+    export default {
+        name: 'Snackbar',
+        props: ['text', 'y', 'x', 'show', 'timeout'],
+        data() {
+            return {
+                active: this.show
+            }
+        },
+
+        methods: {
+            closeSnackBar () {
+                this.$emit('closeSnackbar')
+            }
+        },
+
+        watch: {
+            show( newValue) {
+                this.active = newValue 
+            }
+        }
+    }
+</script>
