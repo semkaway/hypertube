9088060a45d5ab0daba0c7780dfa5e5a8068d113
Pavlo <pkolomiy@gmail.com>
2018-12-07 18:03:15 +0200

    Added `user_id` to  GET  `/api/user/data`

--- backend/src/api/routers/user/rootRouter/data.js
+++ backend/src/api/routers/user/rootRouter/data.js
@@ -2,6 +2,7 @@ export const data = (req, res) => {
     let body = (({first, last, email, pendingEmail, image}) =>
         ({first, last, email, pendingEmail, image}))(req.user);
 
+    body.user_id  = req.user._id;
     body.intra    = req.user.intraId !== null;
     body.github   = req.user.githubId !== null;
     body.password = req.user.password !== null;
