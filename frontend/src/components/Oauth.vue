c7e93352d91e351c03565bb3c13eaa752015f3f9
Valentin Tolochko <vtolochk@gmail.com>
2018-12-10 12:11:03 +0200

    [ADD] Oauth loader

--- frontend/src/components/Oauth.vue
+++ frontend/src/components/Oauth.vue
@@ -1,84 +1,92 @@
 <template>
-  <div>
-
-  </div>
+  <div><Loader :run='runLoader'/></div>
+  
 </template>
 
 <script>
 import {HTTP} from '../http-common';
 import setAuthorizationToken from '../utils/setAuthToken'
+import Loader from './Loader'
 
 export default {
   name: 'UserPage',
+	 components: { Loader },
   data () {
-    return {
-
-      }
+    return { runLoader: false}
     },
     mounted() {
-      const urlParams = new URLSearchParams(window.location.search);
-      const currUrl = window.location.pathname
-      const myCode = urlParams.get('code');
-      const accessDenied = urlParams.get('error');
-      console.log("token: "+ localStorage.token)
+		const urlParams = new URLSearchParams(window.location.search)
+		const currUrl = window.location.pathname
+		const myCode = urlParams.get('code')
+		const accessDenied = urlParams.get('error')
 
-      if(myCode == null && accessDenied == null) {
-        console.log('invalid data')
-        localStorage.token = ''
-        this.$router.push('/')
-      } else if (myCode != null && accessDenied == null) {
-        console.log('not null')
-        if (localStorage.token !== '') {
-          // add user
-		  // need TO DO IT !!!!
-          HTTP.post(`user/add`+currUrl, { "code": myCode })
-            .then(response => {
-              // emit to app vue intra user
-              if (response.data.success == true) {
-                console.log('all good')
-                this.$router.push('/user/settings')
-              } else if (response.data.message == "Invalid token") {
-                localStorage.token = ''
-                this.$router.push('/')
-              } else if (response.data.message == "User exist") {
-                this.$router.push('/user/settings')
-                console.log('this user already exists')
-              }
-            })
-            .catch((err) => {
-              console.log("server error")
-              console.log(err.response.data.error.message)
-              this.$router.push('/')
-            })
-        }
-        else {
-          // login
-          HTTP.post(`user/oauth`+currUrl, { "code": myCode, "locale": this.$i18n.locale }).then(response => {
-              if (response.data.success == true) {
-                this.$i18n.locale = response.data.locale
-                localStorage.locale = response.data.locale
-                setAuthorizationToken(response.data.token)
-                // set user
-				 HTTP.get('user/data/').then(result => {
-					if (result.data.success == false) {
+		this.runLoader = true
+      	if (myCode == null && accessDenied == null) {
+		    this.runLoader = false
+          	setAuthorizationToken(false)
+          	this.$router.push('/')
+      	} else if (myCode != null && accessDenied == null) {
+			if (localStorage.token !== '') { // add user
+				HTTP.post(`user/add` + currUrl, { "code": myCode }).then(response => {
+					if (response.data.success == true) {
+						console.log('all good')
+						this.$router.push('/user/settings')
+					} else if (response.data.message == "Invalid token") {
 						setAuthorizationToken(false)
 						this.$router.push('/')
-					} else {
-						this.$emit('setTokenAndLocale', {token: response.data.token, locale: response.data.locale })
-						this.$emit('updateUser', result.data)
+					} else if (response.data.message == "User exist") {
+						this.$router.push('/user/settings')
+						console.log('this user already exists')
 					}
-				}).catch((err) => { setAuthorizationToken(false); this.$router.push('/')})
-              } else { this.$router.push('login?fail=true') }
-            })
-            .catch((err) => { setAuthorizationToken(false);  this.$router.push('/') })
-        }
-      } else if (myCode == null && accessDenied != null) {
-        if (localStorage.token === '') {
-          this.$router.push('login?fail=true')
-        } else {
-          this.$router.push('user/settings')
-        }
-      } else { setAuthorizationToken(false);  this.$router.push('/') }
+					this.runLoader = false
+				})
+				.catch((err) => {
+					this.runLoader = false
+					this.$router.push('/')
+				})
+        	} // login user
+			else {
+				HTTP.post(`user/oauth`+currUrl, { "code": myCode, "locale": this.$i18n.locale }).then(response => {
+					if (response.data.success == true) {
+						this.$i18n.locale = response.data.locale
+						localStorage.locale = response.data.locale
+						setAuthorizationToken(response.data.token)
+						HTTP.get('user/data/').then(result => {
+							if (result.data.success == false) {
+								setAuthorizationToken(false)
+								this.$router.push('/')
+							} else {
+								this.$emit('setTokenAndLocale', {token: response.data.token, locale: response.data.locale })
+								this.$emit('updateUser', result.data)
+							}
+							this.runLoader = false
+						}).catch((err) => { 
+							setAuthorizationToken(false)	
+							this.runLoader = false 
+							this.$router.push('/')
+						})
+					} else { 	
+						this.runLoader = false
+						this.$router.push('login?fail=true') 
+					}
+				}).catch((err) => { 
+					setAuthorizationToken(false); 
+					this.runLoader = false; 
+					this.$router.push('/')
+				})
+			}
+		} else if (myCode == null && accessDenied != null) {
+			if (localStorage.token === '') {
+				this.$router.push('login?fail=true')
+			} else {
+				this.$router.push('user/settings')
+			}
+			this.runLoader = false
+      } else {
+		  setAuthorizationToken(false)
+		  this.runLoader = false
+		  this.$router.push('/') 
+		}
     },
   }
 </script>
--- frontend/src/components/includes/TheHeader.vue
+++ frontend/src/components/includes/TheHeader.vue
@@ -9,7 +9,7 @@
         <v-toolbar-items v-if='userLoggedIn === true'>
             <v-menu color="grey darken-3" dark bottom origin="center center" transition="scale-transition">
             <v-btn flat slot="activator">
-                <v-img class='rounded round-img' :aspect-ratio="16/9" height='40px' width='40px' :src="headerUser.image == '' ? 'http://www.studioclio.com.br/sites/default/files/imagens/evento/pitagoras_0.jpg' : headerUser.image" alt="User photo"></v-img>
+                <v-img class='rounded round-img' :aspect-ratio="16/9" height='40px' width='40px' :src="headerUser.image" alt="User photo"></v-img>
                 <span class='ml-2 header-name'>{{ headerUser.first | capitalizeFirstLetter }}</span>
             </v-btn>
             <v-list>
