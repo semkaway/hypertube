a8438edd0e1db09be0225329b2e04a70fcbee377
semkaway <semkaway@gmail.com>
2018-12-10 16:36:38 +0200

    [UPD] user profile

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -4,58 +4,33 @@
       <v-flex d-flex xs12 sm6 md2>
       </v-flex>
       <v-flex xs12 sm6 md8 class="mt-5">
-        <div flat width="100%">
-          <v-img :aspect-ratio="16/9" :src="bgImg | image">
+        <div flat>
+          <v-img :aspect-ratio="13/6" :src="bgImg | image">
             <v-layout pa-2 column fill-height class="lightbox white--text">
               <v-spacer></v-spacer>
               <v-layout shrink>
                 <v-flex shrink>
                   <v-avatar size="90" tile style="border: 8px solid white; border-radius: 50%;" class="p-5 mb-3 ml-3">
                     <img  class='rounded round-img'
+                          style="position: absolute;"
                           :aspect-ratio="16/9"
-                          :src="user.image">
+                          :src="myUser.image">
                   </v-avatar>
                 </v-flex>
                 <v-flex shrink>
-                  <p class="display-3">{{user.first}} {{user.last}}</p>
+                  <p class="display-3">{{myUser.first}} {{myUser.last}}</p>
                 </v-flex>
               </v-layout>
             </v-layout>
           </v-img>
         </div>
-        <div class="display-3 text-center">Commented movies</div>
-        <div style="position: relative">
-          <v-btn
-                absolute
-                dark
-                fab
-                top
-                right
-                color="pink"
-              >
-                <v-icon>add</v-icon>
-              </v-btn>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex v-for="pic in commentedMovies" xs12 md4 lg3>
-                  <v-img  :aspect-ratio="1/1.5"
-                          max-width="230"
-                          :src="pic">
-                  </v-img>
-                </v-flex>
-              </v-layout>
-            </v-container>
-            <v-btn
-                absolute
-                dark
-                fab
-                top
-                right
-                color="pink"
-              >
-                <v-icon>add</v-icon>
-              </v-btn>
-            </div>
+        <list-of-movies   :title="$t('profile.profile.commented')"
+                          :movies='commentedMovies'
+                          :totalNumberOfMovies="totalNumberOfCommentedMovies"></list-of-movies>
+        <list-of-movies   v-if="currentUser"
+                          :title="$t('profile.profile.watched')"
+                          :movies='watchedMovies'
+                          :totalNumberOfMovies="totalNumberOfWatchedMovies"></list-of-movies>
       </v-flex>
       <v-flex d-flex xs12 sm6 md2>
       </v-flex>
@@ -67,38 +42,69 @@
 
 import {HTTP} from '../http-common';
 import randomImage from '../utils/randomImage'
+import listOfMovies from './listOfMovies'
 
 export default {
   name: 'UserPage',
   filters: {
     image: randomImage
   },
+  props: ['user'],
+  components: {'list-of-movies': listOfMovies},
   data () {
     return {
-      user: {},
-      commentedMovies: [
-        'https://images.pexels.com/photos/1285625/pexels-photo-1285625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'https://images.pexels.com/photos/450597/pexels-photo-450597.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'https://images.pexels.com/photos/408503/pexels-photo-408503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'https://images.pexels.com/photos/618545/pexels-photo-618545.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'https://images.pexels.com/photos/1051073/pexels-photo-1051073.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'https://images.pexels.com/photos/417344/pexels-photo-417344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'https://images.pexels.com/photos/287229/pexels-photo-287229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'https://images.pexels.com/photos/1313814/pexels-photo-1313814.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-      ],
-      bgImg: ''
+      myUser: {},
+      commentedMovies: [],
+      watchedMovies: [],
+      bgImg: '',
+      totalNumberOfCommentedMovies: 0,
+      totalNumberOfWatchedMovies: 0,
+      currentUser: false
       }
     },
       mounted() {
+        let user_id = window.location.pathname.split('/')[2]
         HTTP
-          .get('user/data/')
+          .get('user/commented-movies/'+user_id)
           .then(result => {
             console.log(result)
             if (result.data.success == true) {
-              this.user = result.data
-              console.log("email: "+this.user.email+" pendingEmail: "+this.user.pendingEmail+" intra: "+this.user.intra+" git: "+this.user.github+" pass: "+this.user.password)
-              if (this.user.image === null) {
-                this.user.image = "https://images.pexels.com/photos/248280/pexels-photo-248280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              if (user_id === this.user.user_id) {
+                HTTP.get('/movie/watched').then(result => {
+                  if (result.data.success == true) {
+                    this.currentUser = true
+                    for (var i = 0; i < result.data.movies.length; i++) {
+        							if (result.data.movies[i].poster_path == null) {
+        								result.data.movies[i].poster_path = 'https://images.pexels.com/photos/1612462/pexels-photo-1612462.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+        							} else {
+        								result.data.movies[i].poster_path = 'http://image.tmdb.org/t/p/original/'+result.data.movies[i].poster_path
+        							}
+            				}
+                    this.watchedMovies = result.data.movies
+                    this.totalNumberOfwatchedMovies = this.watchedMovies.length
+                  } else if (result.data.success == false) {
+                    setAuthorizationToken(false)
+                    this.$router.push('/')
+                  }
+                })
+                .catch((err) => {
+                  console.log(err)
+                })
+              }
+              this.myUser.first = result.data.first
+              this.myUser.last = result.data.last
+              this.myUser.image = result.data.image
+              for (var i = 0; i < result.data.movies.length; i++) {
+  							if (result.data.movies[i].poster_path == null) {
+  								result.data.movies[i].poster_path = 'https://images.pexels.com/photos/1612462/pexels-photo-1612462.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  							} else {
+  								result.data.movies[i].poster_path = 'http://image.tmdb.org/t/p/original/'+result.data.movies[i].poster_path
+  							}
+      				}
+              this.commentedMovies = result.data.movies
+              this.totalNumberOfCommentedMovies = this.commentedMovies.length
+              if (this.myUser.image === null) {
+                this.myUser.image = "https://images.pexels.com/photos/248280/pexels-photo-248280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
               }
             } else if (result.data.success == false) {
               setAuthorizationToken(false)
@@ -107,41 +113,10 @@ export default {
           })
           .catch((err) => {
             console.log(err)
-            localStorage.token = ''
-            this.$router.push('/')
           })
-        // let user_id = window.location.pathname.split('/')[2]
-        // HTTP
-        //   .get('user/commented-movies/'+user_id)
-        //   .then(result => {
-        //     console.log(result)
-            // for(let i = 0; i < result.data.movieIds.length; i++) {
-            //   HTTP.get('https://api.themoviedb.org/3/movie/'+result.data.movieIds[i]+'?api_key=09665afd54623c9413c3f9336484b01c&language='+localStorage.locale)
-            //   .then(movie => {
-            //     this.commentedMovies.push(movie)
-            //   })
-            //   .catch((err) => {
-            //     console.log(err)
-            //     console.log('tmdb error')
-            //   })
-            // }
-            // console.log(this.commentedMovies)
-            // if (result.data.success == true) {
-            //   this.user = result.data
-            //   console.log("email: "+this.user.email+" pendingEmail: "+this.user.pendingEmail+" intra: "+this.user.intra+" git: "+this.user.github+" pass: "+this.user.password)
-            //   if (this.user.image === null) {
-            //     this.user.image = "https://images.pexels.com/photos/248280/pexels-photo-248280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            //   }
-            // } else if (result.data.success == false) {
-            //   setAuthorizationToken(false)
-            //   this.$router.push('/')
-            // }
-          // })
-          // .catch((err) => {
-          //   console.log(err)
-          // })
       },
       methods: {
+
       }
 
   }
@@ -149,9 +124,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.profilePic {
-  border: 5px white;
-}
 
 </style>
---
+++ frontend/src/components/listOfMovies.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <div class="display-1 font-weight-bold m-2">{{title}}</div>
+    <div style="position: relative">
+        <v-container grid-list-md class="p-2">
+          <v-layout wrap relative>
+            <v-btn  class="hidden-sm-and-down"
+                    v-if="startMoviesToShow != 0"
+                    absolute
+                    dark
+                    fab
+                    depressed
+                    left
+                    color="grey lighten-2"
+                    style="top: 45%; opacity: 0.8;"
+                    @click.prevent="showPrevious"
+                >
+                  <v-icon color="black">keyboard_arrow_left</v-icon>
+                </v-btn>
+            <v-flex v-for="(movie,index) in movies"
+                    v-if="index >= startMoviesToShow && index < endMoviesToShow"
+                    :key="index"
+                    xs12 md4 lg3>
+              <router-link :to="'/movies/'+movie.id">
+                <v-img  :aspect-ratio="1/1.5"
+                        max-width="100%"
+                        :src="movie.poster_path">
+                          <v-layout slot="placeholder"
+                                    fill-height
+                                    align-center
+                                    justify-center
+                                    ma-0
+                                    >
+                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                          </v-layout>
+                </v-img>
+              </router-link>
+            </v-flex>
+            <v-btn  class="hidden-sm-and-down"
+                    v-if="endMoviesToShow < totalNumberOfMovies"
+                    absolute
+                    dark
+                    fab
+                    depressed
+                    right
+                    color="grey lighten-2"
+                    style="top: 45%; opacity: 0.8;"
+                    @click.prevent="showNext"
+                >
+                  <v-icon color="black">keyboard_arrow_right</v-icon>
+                </v-btn>
+          </v-layout>
+        </v-container>
+        <div class="text-xs-center hidden-md-and-up">
+        <v-btn  v-if="endMoviesToShow < totalNumberOfMovies"
+                @click.prevent="addMore"
+                depressed
+                color="grey"
+                class="white--text mt-3">{{$t('button.showMore')}}</v-btn>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'listOfMovies',
+    data () {
+      return {
+        startMoviesToShow: 0,
+        endMoviesToShow: 4
+      }
+    },
+    methods: {
+        showPrevious() {
+          if (this.endMoviesToShow == this.totalNumberOfMovies) {
+            console.log('yo,here')
+            this.startMoviesToShow -= 4
+            this.endMoviesToShow -= this.totalNumberOfMovies % 4
+          } else {
+            this.startMoviesToShow -= 4
+            this.endMoviesToShow -= 4
+          }
+          console.log('mod: ', this.totalNumberOfMovies % 4)
+        },
+        showNext() {
+          if (this.endMoviesToShow + 4 > this.totalNumberOfMovies) {
+            this.startMoviesToShow += 4
+            this.endMoviesToShow = this.totalNumberOfMovies
+          } else {
+            this.startMoviesToShow += 4
+            this.endMoviesToShow += 4
+          }
+          console.log('this.totalNumberOfMovies: ', this.totalNumberOfMovies)
+          console.log('showing next')
+        },
+        addMore() {
+          if (this.endMoviesToShow + 4 > this.totalNumberOfMovies) {
+            this.endMoviesToShow = this.totalNumberOfMovies
+          } else {
+            this.endMoviesToShow += 4
+          }
+        }
+    },
+    props: ['movies', 'totalNumberOfMovies', 'title']
+}
+
+</script>
+
+<style scoped>
+
+a {
+  text-decoration: none;
+}
+
+</style>
