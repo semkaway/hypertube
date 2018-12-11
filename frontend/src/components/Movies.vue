b156cd9a1d6d7a05a7cde4e808462bd557276ce5
Valentin Tolochko <vtolochk@gmail.com>
2018-12-11 10:37:32 +0200

    [UPD] api key to constants

--- frontend/src/components/Movies.vue
+++ frontend/src/components/Movies.vue
@@ -12,6 +12,8 @@
           @keyup.native.enter="searchMovies"
           @click:append-outer="searchMovies"
         ></v-text-field>
+		<v-btn @click='test'>search</v-btn>
+
         <div v-if="notFound">{{ $t('movies.notFound') }}</div>
         <v-container grid-list-md>
           <v-layout row wrap class="mt-3">
@@ -70,9 +72,11 @@
 </template>
 
 <script>
-    import {HTTP} from '../http-common'
-    import setAuthorizationToken from '../utils/setAuthToken'
+	import axios from 'axios'
 	import NotFound from './NotFound'
+	import { HTTP } from '../http-common'
+	import * as constants from '../utils/constants'
+    import setAuthorizationToken from '../utils/setAuthToken'
 
   export default {
     name: 'Movies',
@@ -83,61 +87,72 @@
         movies: [],
         page: 1,
         searchParams: '',
-        query: `https://api.themoviedb.org/3/movie/popular?api_key=09665afd54623c9413c3f9336484b01c&language=`
-        +localStorage.locale+'&append_to_response=images&include_image_language='+localStorage.locale+',null&page=',
+        query: `https://api.themoviedb.org/3/movie/popular?api_key=${constants.API_KEY}&language=${this.locale}&append_to_response=images&include_image_language=${this.locale},null&page=`,
         totalPages: 1,
-        notFound: false
+        notFound: false,
+		
       }
     },
     methods: {
-      requestMovies() {
-        console.log('this.query + this.page =>', this.query + this.page)
-        HTTP.get(this.query + this.page).then(result => {
-          console.log(result)
-          if (result.data.total_results == 0) {
-            this.notFound = true
-            this.totalPages = 1
-            return false
-          }
-          for (var i = 0; i < result.data.results.length; i++) {
-            for(var key in result.data.results[i]) {
-              if (key == 'release_date') {
-                var year = result.data.results[i][key].split('-')
-                result.data.results[i][key] = year[0]
-              }
-              if (key == 'poster_path') {
-                if (result.data.results[i][key] == null) {
-                  result.data.results[i][key] = 'https://images.pexels.com/photos/1612462/pexels-photo-1612462.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-                } else {
-                  result.data.results[i][key] = 'http://image.tmdb.org/t/p/w500'+result.data.results[i][key]
-                }
-              }
-            }
-            this.movies.push(result.data.results[i])
-          }
-          this.totalPages = result.data.total_pages
-        }).catch((e) => {
-          console.log('e', e)
-        })
-      },
-      showMore () {
-        if (this.page + 1 < this.totalPages)
-          this.page = this.page + 1;
-        else
-          this.page = this.totalPages;
-        this.requestMovies()
-      },
-      searchMovies() {
-      console.log(this.searchParams)
-        if (this.searchParams !== '') {
-          this.query = `https://api.themoviedb.org/3/search/movie?api_key=09665afd54623c9413c3f9336484b01c&language=`
-          + localStorage.locale + '&query='+this.searchParams + '&images&include_image_language=' + localStorage.locale + ',null' +
-          '&page='
-          this.movies = []
-          this.page = 1
-          this.requestMovies()
-        }
-      }
+      	requestMovies() {
+			HTTP.get(this.query + this.page).then(result => {
+			console.log(result)
+				if (result.data.total_results == 0) {
+					this.notFound = true
+					this.totalPages = 1
+					return false
+				}
+				for (var i = 0; i < result.data.results.length; i++) {
+					for(var key in result.data.results[i]) {
+					if (key == 'release_date') {
+						var year = result.data.results[i][key].split('-')
+						result.data.results[i][key] = year[0]
+					}
+					if (key == 'poster_path') {
+						if (result.data.results[i][key] == null) {
+							result.data.results[i][key] = 'https://images.pexels.com/photos/1612462/pexels-photo-1612462.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+						} else {
+							result.data.results[i][key] = 'http://image.tmdb.org/t/p/w500' + result.data.results[i][key]
+						}
+					}
+					}
+					this.movies.push(result.data.results[i])
+				}
+				this.totalPages = result.data.total_pages
+			}).catch((e) => { console.log('e', e) })
+      	},
+
+      	showMore () {
+			if (this.page + 1 < this.totalPages)
+			this.page = this.page + 1;
+			else
+			this.page = this.totalPages;
+			this.requestMovies()
+      	},
+
+		searchMovies() {
+			console.log(this.searchParams)
+			if (this.searchParams !== '') {
+				this.query = `https://api.themoviedb.org/3/search/movie?api_key=${constants.API_KEY}&language=${this.locale}&query=${this.searchParams}&images&include_image_language=${this.locale},null&page=`
+				this.movies = []
+				this.page = 1
+				this.requestMovies()
+			}
+		},
+
+		test() {
+			delete axios.defaults.headers.common['Authorization']
+
+			HTTP.get(`https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=${constants.API_KEY}`).then(response => {
+				console.log('response =>', response)
+			}).catch(error => {
+				console.log('error =>', error)
+			})
+
+			setAuthorizationToken(this.token)
+		}
+
+
     },
     mounted () {
         this.requestMovies()
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,22 +1,23 @@
 
-const GENRES = [
-    {id: 28, name: "Action"},
-    {id: 12, name: "Adventure"},
-    {id: 16, name: "Animation"},
-    {id: 35, name: "Comedy"},
-    {id: 80, name: "Crime"},
-    {id: 99, name: "Documentary"},
-    {id: 18, name: "Drama"},
-    {id: 10751, name: "Family"},
-    {id: 14, name: "Fantasy"},
-    {id: 36, name: "History"},
-    {id: 27, name: "Horror"},
-    {id: 10402, name: "Music"},
-    {id: 9648, name: "Mystery"},
-    {id: 10749, name: "Romance"},
-    {id: 878, name: "Science Fiction"},
-    {id: 10770, name: "TV Movie"},
-    {id: 53, name: "Thriller"},
-    {id: 10752, name: "War"},
-    {id: 37, name: "Western"}
+export const API_KEY = '09665afd54623c9413c3f9336484b01c'
+export const GENRES = [
+        {id: 28, name: "Action"},
+        {id: 12, name: "Adventure"},
+        {id: 16, name: "Animation"},
+        {id: 35, name: "Comedy"},
+        {id: 80, name: "Crime"},
+        {id: 99, name: "Documentary"},
+        {id: 18, name: "Drama"},
+        {id: 10751, name: "Family"},
+        {id: 14, name: "Fantasy"},
+        {id: 36, name: "History"},
+        {id: 27, name: "Horror"},
+        {id: 10402, name: "Music"},
+        {id: 9648, name: "Mystery"},
+        {id: 10749, name: "Romance"},
+        {id: 878, name: "Science Fiction"},
+        {id: 10770, name: "TV Movie"},
+        {id: 53, name: "Thriller"},
+        {id: 10752, name: "War"},
+        {id: 37, name: "Western"}
 ]
