be9330c973404a36a15ba36ee6c97368b76278cb
vliubko <neznam.ua@gmail.com>
2018-12-11 20:41:15 +0200

    [ADD] Subtitle working good. TODO: locale for subs

--- backend/src/api/routers/movie/index.js
+++ backend/src/api/routers/movie/index.js
@@ -5,7 +5,6 @@ import {stream} from "./stream";
 import {one} from "./one";
 import {decodeToken} from "../utils/decodeToken";
 import {comment} from "./comment";
-import {subtitle} from './subtitle'
 
 export const movieRouter = express.Router();
 
@@ -16,5 +15,3 @@ movieRouter.route('/one/:id').get(decodeToken, one(User));
 movieRouter.route('/stream/:id').get(stream(User));
 
 movieRouter.route('/comment').post(decodeToken, comment);
-
-movieRouter.route('/subtitle').post(subtitle)
