da9dbdc97f2062af8b0ce2f6f06d6218020a0345
Valentin Tolochko <vtolochk@gmail.com>
2018-12-12 20:40:51 +0200

    [FIX] header language bug

--- frontend/src/components/includes/TheHeader.vue
+++ frontend/src/components/includes/TheHeader.vue
@@ -41,7 +41,7 @@
                 <v-menu color="grey darken-3" dark bottom origin="center center" transition="scale-transition">
                     <v-btn flat slot="activator">
                         <v-icon>language</v-icon>
-                        <span class='ml-2'>{{ locale }}</span>
+                        <span class='ml-2'>{{ headerLocale }}</span>
                     </v-btn>
                     <v-list>
                         <v-list-tile v-for="(item, i) in lang" :key="i" @click="changeLanguage(item.short)">
