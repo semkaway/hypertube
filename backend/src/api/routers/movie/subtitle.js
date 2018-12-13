007e439024580e0c6816104f7331d1ab819eece5
vliubko <vadym.liubko@depositphotos.com>
2018-12-13 13:59:09 +0200

    [UPD] subs reworked

--- backend/src/api/routers/movie/subtitle.js
+++ backend/src/api/routers/movie/subtitle.js
@@ -42,8 +42,6 @@ const getFile = async (srtDownloadUrl, filename) => {
     return file
 }
 
-var subsArray = []
-
 export const getSubtitle = async (imdbid, locale) => {
     await mkdirp(SUBTITLE_STORAGE, function (err) {
         if (err) console.error(err)
@@ -56,17 +54,20 @@ export const getSubtitle = async (imdbid, locale) => {
     })
 
     if (resp.en) {
-        subsArray.en = {
+        var enSubs = {
+            locale: "en",
             srtDownloadUrl: resp.en.url,
             file: await getFile(resp.en.url, resp.en.filename)
         }
     }
 
     if (resp.ru) {
-        subsArray.ru = {
+        var ruSubs = {
+            locale: "ru",
             srtDownloadUrl: resp.ru.url,
             file: await getFile(resp.ru.url, resp.ru.filename)
         }
     }
+    var subsArray = {enSubs, ruSubs}
     return subsArray
 }
